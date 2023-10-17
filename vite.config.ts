cdbb75f578a4d16e4956e13483ae7214a4afb8ca
Yago Moreira <yagomoreira@gmail.com>
2023-10-17 14:01:22 -0300

    Configuração do Ambiente de Testes

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
@@ -6,5 +8,16 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['js-big-decimal']
-  }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.ts',
+    css: true,
+    reporters: ['verbose'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      provider: 'v8'
+    }
+  },
 })
